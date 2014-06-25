e474a40339e8615f331fd9d0483b463059130bbc
Trevor Ewen <tewen@neosavvy.com>
2014-06-25 19:49:34 -0400

    Added the good old post request.

--- server/app.js
+++ server/app.js
@@ -1,14 +1,16 @@
 var http = require('http'),
     express = require('express'),
-    path = require('path');
+    path = require('path'),
+    bodyParser = require('body-parser');
 
 var app = express();
 app.set('port', process.env.PORT || 4000);
 app.use(express.static(path.join(__dirname, '../')));
+app.use(bodyParser.json());
 
-//app.get('/', function (req, res) {
-//    res.send('<html><body><h1>Hello World</h1></body></html>');
-//});
+app.post('/post-service-example', function (req, res) {
+   res.send("You made a post of: " + JSON.stringify(req.body));
+});
 
 http.createServer(app).listen(app.get('port'), function(){
     console.log('Express server listening on port ' + app.get('port'));
