164e1e9638c90c466f09655a81ab18ec64838ab3
Trevor Ewen <tewen@neosavvy.com>
2014-06-25 20:32:24 -0400

    Added the post request validation.

--- app/app.js
+++ app/app.js
@@ -1,39 +1,22 @@
-angular.module('questions', []).constant('Data', {
-    questions: [
-        {question: 'How often do you watch sports?', options: [
-            {value: 'Everyday'},
-            {value: 'Often'},
-            {value: 'Some days'},
-            {value: 'Rarely'},
-            {value: 'Never'}
-        ]},
-        {question: 'What is the best pet for your home?', options: [
-            {value: 'Cat'},
-            {value: 'Dog'},
-            {value: 'Fish'},
-            {value: 'Rabbit'},
-            {value: 'Other'}
-        ]},
-        {question: 'What is your preferred bank?', options: [
-            {value: 'Chase'},
-            {value: 'Citi'},
-            {value: 'Bank of America'},
-            {value: 'Wells Fargo'},
-            {value: 'Regional'},
-            {value: 'Online'},
-            {value: 'Other'}
-        ]},
-        {question: 'How many siblings do you have?', options: [
-            {value: 'More than 4'},
-            {value: '4'},
-            {value: '3'},
-            {value: '2'},
-            {value: '1'},
-            {value: 'None'}
-        ]},
-        {question: 'Do you eat seafood?', options: [
-            {value: true},
-            {value: false}
-        ]}
-    ]
-});
+angular.module('http', []).controller('PostCtrl',
+    ['$scope', '$http',
+        function ($scope, $http) {
+            $scope.model = {};
+            $scope.postTerms = [
+                "name",
+                "band",
+                "instrument",
+                "country",
+                "influence"
+            ];
+
+            $scope.onUpdate = function () {
+                $http({
+                    method: 'POST',
+                    url: '/post-service-example',
+                    data: $scope.model
+                }).success(function (response) {
+                    $scope.recentPost = JSON.stringify(response);
+                });
+            };
+        }]);
--- app/controllers/detail.js
+++
@@ -1,9 +0,0 @@
-angular.module('questions').controller('Detail',
-    ['$scope',
-        function ($scope) {
-
-            $scope.onSelectAnswer = function (question, option) {
-                question.answer = option;
-            };
-
-        }]);
